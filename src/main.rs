089ec1af753c586ec9861b254a72e01da3dcbbea
Andrew Plaza <aplaza@liquidthink.net>
2017-05-14 00:31:04 -0400

    complete iterator

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate hyper;
 
 use serde_xml_rs::deserialize;
 use hyper::Client;
+use hyper::Url;
 use std::io::Read;
 
 #[derive(Debug, Deserialize)]
@@ -40,33 +41,35 @@ struct Data {
 }
 
 enum Hubs {
-    TheForge:   { id: i64, system: "Jita",              hub_id: i64},
-    Domain:     { id: i64, system: "Amarr",             hub_id: i64},
-    Heimatar:   { id: i64, system: "Rens",              hub_id: i64},
-    SinqLaison: { id: i64, system: "Dodixie",           hub_id: i64},
-    Metropolis: { id: i64, system: "Hek",               hub_id: i64},
-    Essence:    { id: i64, system: "Oursulaert",        hub_id: i64},
-    TashMurkon: { id: i64, system: "Tash-Murkon Prime", hub_id: i64},
-    Khanid:     { id: i64, system: "Agil",              hub_id: i64},
+    TheForge:   { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Jita", },
+    Domain:     { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Amarr"},
+    Heimatar:   { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Rens"},
+    SinqLaison: { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Dodixie"},
+    Metropolis: { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Hek"},
+    Essence:    { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Oursulaert"},
+    TashMurkon: { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Tash-Murkon Prime"},
+    Khanid:     { RegionLimit("regionlimit", i64), System("usesystem", i64), SystemName: "Agil"},
 }
 
 fn main() {
     let url = "http://api.eve-central.com/api/marketstat?typeid=34&typeid=35&regionlimit=10000002";
-    let client = Client::new();
 
-    let mut res = client.get(url).send().unwrap();
-    assert_eq!(res.status, hyper::Ok, "[ERROR NOT OK] The API seems to be down");
-    
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    let eve_api: EvecApi = deserialize(body.as_bytes()).unwrap();
-    println!("{:#?}", eve_api);
+
 }
 
 fn request_data(reg_id: i64, sys_id: i64, t_ids: Vec<i64>, ) {
     let client = Client::new(); 
+    
+    let mut endpoint = Url::parse_with_params("http://api.eve-central.com/api/marketstat",
+                                              &[]
+                                              );
+    match endpoing {
+        Some() 
+    }
+    
 
     let mut res = client.get(form_url(reg_id, sys_id, t_ids)).send().unwrap();
+    
     assert_eq!(res.status, hyper::ok, "[ERROR] The API Request Did Not Work");
     
     let mut body = String::new();
@@ -76,7 +79,7 @@ fn request_data(reg_id: i64, sys_id: i64, t_ids: Vec<i64>, ) {
 }
 
 fn form_url(reg_id: i64, sys_id: i64, t_ids: Vec<i64>) -> String {
-    unimplemented!();
+    let base_url = "http://api.eve-central.com/api/marketstat?typeid=34&typeid=35&regionlimit=";
 }
 
 
